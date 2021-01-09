9f218acd21e44c9eb927cca390124c5b9fbab2c6
nlmuscanell <nicolemuscanell@gmail.com>
2021-01-09 18:15:51 -0500

    js, html, and app updates

--- app.py
+++ app.py
@@ -44,7 +44,6 @@ def index():
     ##return("hello")
     return render_template("index.html")
 
-
 # Define the route to "/nutrition"
 @app.route("/api/v1.0/nutrition")
 
@@ -156,7 +155,156 @@ def nutrition():
 
     
     return jsonify(item_names_dict)
+
+# Define the route to "/restaurants"
+@app.route("/api/v1.0/restaurants")
+
+def restaurants(): 
+    #Create the session (link) from Python to the DB
+    session= Session(engine)
+
+    sel = [Menu_items.item_name, Categories.category_name, Restaurants.name]
+    nutritional_info = session.query(*sel).filter(Nutrition.restaurant_id==Restaurants.id).\
+        filter(Nutrition.category_id==Categories.id).\
+        filter(Nutrition.item_id==Menu_items.id).all()
     
+    # Create dictionaries to store the information
+    # Main restaurant dictionary
+    restaurants = {}
+
+    # Dictionaries for each restaurants
+    mcdonalds_categories_dic = {}
+    subway_categories_dic = {}
+    starbucks_categories_dic = {}
+
+    # Lists for Mcdonalds categories
+    mc_desserts =[]
+    mc_beverages = []
+    mc_breakfast = []
+    mc_poultry_fish_sandwich_wraps = []
+    mc_salads = []
+    mc_snacks_and_sides = [ ]
+    mc_beef_and_pork_sandwich_wraps = []
+
+   # Lists for Subway categories
+    sb_desserts =[]
+    sb_bread_toppings = []
+    sb_breakfast = []
+    sb_poultry_fish_sandwich_wraps = []
+    sb_salads = []
+    sb_beef_and_pork_sandwich_wraps = []
+    sb_veggie_sandwich_wraps=[]
+
+  # Lists for Starbucks categories
+    stb_beverages = []
+    stb_veggie_sandwich_wraps=[]
+    stb_poultry_fish_sandwich_wraps = []
+    stb_breakfast = []
+    stb_salads = []
+    stb_bread_toppings = []
+    stb_desserts =[]
+    stb_beef_and_pork_sandwich_wraps = []
+    stb_other_food = []
+
+    for item in nutritional_info:
+        for restaurant in item:
+           if item[2] == "mcdonalds":
+               if item[1] == "desserts":
+                   mc_desserts.append(item[0])
+               elif item[1] == "beverages":
+                   mc_beverages.append(item[0])
+               elif item[1] == "breakfast":
+                   mc_breakfast.append(item[0])
+               elif item[1] == "poultry & fish sandwich/wraps":
+                   mc_poultry_fish_sandwich_wraps.append(item[0])
+               elif item[1] == "salads":
+                   mc_salads.append(item[0])
+               elif item[1] == "snacks & sides":
+                   mc_snacks_and_sides.append(item[0])
+               elif item[1] == "beef & pork sandwich/wraps":
+                   mc_beef_and_pork_sandwich_wraps.append(item[0])
+           elif item[2] == "subway":
+                if item[1] == "desserts":
+                    sb_desserts.append(item[0])
+                elif item[1] == "bread & toppings":
+                    sb_bread_toppings.append(item[0])
+                elif item[1] == "breakfast":
+                    sb_breakfast.append(item[0])
+                elif item[1] == "poultry & fish sandwich/wraps":
+                    sb_poultry_fish_sandwich_wraps.append(item[0])
+                elif item[1] == "salads":
+                    sb_salads.append(item[0])
+                elif item[1] == "beef & pork sandwich/wraps":
+                    sb_beef_and_pork_sandwich_wraps.append(item[0])
+                elif item[1] == "veggie sandwich/wraps":
+                    sb_veggie_sandwich_wraps.append(item[0])
+           elif item[2] == "starbucks":
+                if item[1] == "beverages":
+                    stb_beverages.append(item[0])
+                elif item[1] == "veggie sandwich/wraps":
+                    stb_veggie_sandwich_wraps.append(item[0])
+                elif item[1] == "poultry & fish sandwich/wraps":
+                    stb_poultry_fish_sandwich_wraps.append(item[0])
+                elif item[1] == "breakfast":
+                    stb_breakfast.append(item[0])
+                elif item[1] == "salads":
+                    stb_salads.append(item[0])
+                elif item[1] == "bread & toppings":
+                    stb_bread_toppings.append(item[0])
+                elif item[1] == "desserts":
+                    stb_desserts.append(item[0])
+                elif item[1] == "beef & pork sandwich/wraps":
+                    stb_beef_and_pork_sandwich_wraps.append(item[0])
+                elif item[1] == "other food":
+                    stb_other_food.append(item[0])
+    # Upper Case Function
+    def upper_case_first_letter(str):
+        return str.title()
+
+    # List Formatting Function
+    def list_formatting(lst):
+        return (sorted(list(set(map(upper_case_first_letter, lst)))))
+
+    # Add the list of categories to the McDonalds dictionary
+    mcdonalds_categories_dic["Desserts"] = list_formatting(mc_desserts)
+    mcdonalds_categories_dic["Beverages"] = list_formatting(mc_beverages)
+    mcdonalds_categories_dic["Breakfast"] = list_formatting(mc_breakfast)
+    mcdonalds_categories_dic["Poultry & Fish Sandwich/Wraps"] = list_formatting(mc_poultry_fish_sandwich_wraps)
+    mcdonalds_categories_dic["Salads"] = list_formatting(mc_salads)
+    mcdonalds_categories_dic["Beef and Pork Sandwich/Wraps"] = list_formatting(mc_beef_and_pork_sandwich_wraps)
+    mcdonalds_categories_dic["Snacks & Sides"] = list_formatting(mc_snacks_and_sides)
+
+    
+    # Add the list of categories to the Subway dictionary
+    subway_categories_dic["Desserts"] = list_formatting(sb_desserts)
+    subway_categories_dic["Bread & Toppings"] = list_formatting(sb_bread_toppings)
+    subway_categories_dic["Breakfast"] = list_formatting(sb_breakfast)
+    subway_categories_dic["Poultry & Fish Sandwich/Wraps"] = list_formatting(sb_poultry_fish_sandwich_wraps)
+    subway_categories_dic["Salads"] = list_formatting(sb_salads)
+    subway_categories_dic["Beef and Pork Sandwich/Wraps"] = list_formatting(sb_beef_and_pork_sandwich_wraps)
+    subway_categories_dic["Veggie & Sandwich/Wraps"] = list_formatting(sb_veggie_sandwich_wraps)
+
+    # Add the list of categories to the Startbuck dictionary
+    starbucks_categories_dic["Beverages"] = list_formatting(stb_beverages)
+    starbucks_categories_dic["Veggie & Sandwich/Wraps"] = list_formatting(stb_veggie_sandwich_wraps)
+    starbucks_categories_dic["Poultry & Fish Sandwich/Wraps"] = list_formatting(stb_poultry_fish_sandwich_wraps)
+    starbucks_categories_dic["Breakfast"] = list_formatting(stb_breakfast)
+    starbucks_categories_dic["Salads"] = list_formatting(stb_salads)
+    starbucks_categories_dic["Bread & Toppings"] = list_formatting(stb_bread_toppings)
+    starbucks_categories_dic["Desserts"] = list_formatting(stb_desserts)
+    starbucks_categories_dic["Beef and Pork Sandwich/Wraps"] = list_formatting(stb_beef_and_pork_sandwich_wraps)
+    starbucks_categories_dic["Other_Food"] = list_formatting(stb_other_food)
+
+
+    # Add the dictionaries to the main Restaurants ditionary
+    restaurants["McDonalds"] = mcdonalds_categories_dic
+    restaurants["Subway"] = subway_categories_dic
+    restaurants["Starbucks"] = starbucks_categories_dic
+
+    session.close
+
+    return jsonify(restaurants)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/default_plots.js
@@ -0,0 +1,463 @@
+// Javascript Project 2: Fast-food Nutrition Plots//
+
+// Note: McDonald's has no veggie sandwiches, bread/toppings, or other foods
+// Note: Subway has no snacks/sides, beverages, or other foods
+// Note: Starbucks has no snacks/sides
+
+// ///// CAPITALIZE FIRST LETTER FUNCTION /////
+
+function capitalizeFirstLetter(str) {
+  if (str == 'mcdonalds') {
+    return "McDonald's"
+  } else {
+     var splitStr = str.split(' ');
+     for (var i = 0; i < splitStr.length; i++) {
+     splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+     }
+    return splitStr.join(' ')
+   }
+  };
+
+///// LOWERCASE FIRST LETTER FUNCTION /////
+
+function lowerCaseFirstLetter(str) {
+  if (str == "McDonald's") {
+    return "mcdonalds"
+  } else {
+     var splitStr = str.split(' ');
+     for (var i = 0; i < splitStr.length; i++) {
+     splitStr[i] = splitStr[i].charAt(0).toLowerCase() + splitStr[i].substring(1);
+     }
+    return splitStr.join(' ')
+   }
+  };
+
+///////////////////// DEFAULT LEFT GAUGE CHART (A) ////////////////////
+
+d3.json("/api/v1.0/nutrition").then(function(data) {
+      
+  defaultInputA = "egg mcmuffin";
+      
+  var names = data.names;
+        
+  // Find the index for the input item
+  itemIndexA = names.indexOf(defaultInputA);
+    
+  // Find the item in the Nutrition object
+  itemNutritionInformation = data.nutrition[itemIndexA];
+    
+  // Store the name of the item and the restaurant name to use it on the chart
+  itemName = itemNutritionInformation["item_name"];
+  restaurantName = itemNutritionInformation["restaurant"];
+    
+  // Select the % daily values
+  dailyValuesObject = itemNutritionInformation["% daily values"];
+    
+    // Create an array with the value names
+    var valueNames = ["Calcium", "Iron", "Sodium", "Vitamin A", "Vitamin C"];
+    
+    // Create an array with the % daily values
+    var calcium = dailyValuesObject["calcium_%_dv"];
+    var iron = dailyValuesObject["iron_%_dv"];
+    var sodium = dailyValuesObject["sodium_%_dv"];
+    var vitaminA = dailyValuesObject["vitamin_a_%_dv"];
+    var vitaminC = dailyValuesObject["vitamin_c_%_dv"];
+       
+    var values = [calcium, iron, sodium, vitaminA, vitaminC, 100];
+      
+    // Gauge //
+    var data = [23, 34, 67, 93, 56, 100];
+    var dataSet = anychart.data.set(values);
+    var palette = anychart.palettes
+      .distinctColors()
+      .items([
+        '#64b5f6',
+        '#1976d2',
+        '#ef6c00',
+        '#ffd54f',
+        '#455a64',
+        '#96a6a6',
+        '#dd2c00',
+        '#00838f',
+        '#00bfa5',
+        '#ffa000'
+      ]);
+  
+      function dinamicText(i) {
+        if (values[i] == "nan") {
+          return valueNames[i] + ', <span style="">NA</span>'
+        } else {
+          return valueNames[i] + ', <span style="">' + values[i] + '%</span>'
+        }
+     };
+      
+      var makeBarWithBar = function (gauge, radius, i, width) {
+        var stroke = null;
+          gauge
+            .label(i)
+            .text(dinamicText(i))
+            .useHtml(true);
+          gauge
+            .label(i)
+            .hAlign('center')
+            .vAlign('middle')
+            .anchor('right-center')
+            .padding(0, 10)
+            .height(width / 2 + '%')
+            .offsetY(radius + '%')
+            .offsetX(0);
+        
+          gauge
+            .bar(i)
+            .dataIndex(i)
+            .radius(radius)
+            .width(width)
+            .fill(palette.itemAt(i))
+            .stroke(null)
+            .zIndex(5);
+          gauge
+            .bar(i + 100)
+            .dataIndex(5)
+            .radius(radius)
+            .width(width)
+            .fill('#F5F4F4')
+            .stroke(stroke)
+            .zIndex(4);
+        
+          return gauge.bar(i);
+        };
+        
+        anychart.onDocumentReady(function () {
+          var gauge = anychart.gauges.circular();
+          gauge.data(dataSet);
+          gauge
+            .fill('#fff')
+            .stroke(null)
+            .padding(0)
+            .margin(100)
+            .startAngle(0)
+            .sweepAngle(270);
+        
+          var axis = gauge.axis().radius(100).width(1).fill(null);
+          axis
+            .scale()
+            .minimum(0)
+            .maximum(100)
+            .ticks({ interval: 1 })
+            .minorTicks({ interval: 1 });
+          axis.labels().enabled(false);
+          axis.ticks().enabled(false);
+          axis.minorTicks().enabled(false);
+          makeBarWithBar(gauge, 100, 0, 17);
+          makeBarWithBar(gauge, 80, 1, 17);
+          makeBarWithBar(gauge, 60, 2, 17);
+          makeBarWithBar(gauge, 40, 3, 17);
+          makeBarWithBar(gauge, 20, 4, 17);
+        
+          gauge.margin(50);
+          gauge
+            .title()
+            .text(
+              'Daily % of Micronutrient values on' + ' ' +capitalizeFirstLetter(itemName) +
+              '<br/> <span style="color:#929292; font-size: 5px;"></span>'  + 'from ' +
+              (capitalizeFirstLetter(restaurantName))
+            )
+            .useHtml(true);
+          gauge
+            .title()
+            .enabled(true)
+            .hAlign('center')
+            .padding(0)
+            .margin([0, 0, 20, 0]);
+          gauge.container('radialChartA');
+          gauge.draw();
+        
+        });
+      });
+
+//////////////////// DEFAULT RIGHT GAUGE CHART (B) ////////////////////
+
+d3.json("/api/v1.0/nutrition").then(function(data) {
+      
+  defaultInputB = "egg white delight";
+      
+  var names = data.names;
+        
+  // Find the index for the input item
+  itemIndexA = names.indexOf(defaultInputB);
+    
+  // Find the item in the Nutrition object
+  itemNutritionInformation = data.nutrition[itemIndexA];
+    
+  // Store the name of the item and the restaurant name to use it on the chart
+  itemName = itemNutritionInformation["item_name"];
+  restaurantName = itemNutritionInformation["restaurant"];
+    
+  // Select the % daily values
+  dailyValuesObject = itemNutritionInformation["% daily values"];
+  
+  // Create an array with the value names
+  var valueNames = ["Calcium", "Iron", "Sodium", "Vitamin A", "Vitamin C"];
+    
+  // Create an array with the % daily values
+  var calcium = dailyValuesObject["calcium_%_dv"];
+  var iron = dailyValuesObject["iron_%_dv"];
+  var sodium = dailyValuesObject["sodium_%_dv"];
+  var vitaminA = dailyValuesObject["vitamin_a_%_dv"];
+  var vitaminC = dailyValuesObject["vitamin_c_%_dv"];
+       
+  var values = [calcium, iron, sodium, vitaminA, vitaminC, 100];
+  console.log(values)
+
+
+  // Gauge //
+  var data = [23, 34, 67, 93, 56, 100];
+  var dataSet = anychart.data.set(values);
+  var palette = anychart.palettes
+    .distinctColors()
+    .items([
+      '#64b5f6',
+      '#1976d2',
+      '#ef6c00',
+      '#ffd54f',
+      '#455a64',
+      '#96a6a6',
+      '#dd2c00',
+      '#00838f',
+      '#00bfa5',
+      '#ffa000'
+    ]);
+  
+    function dinamicText(i) {
+      if (values[i] == "nan") {
+        return valueNames[i] + ', <span style="">NA</span>'
+      } else {
+        return valueNames[i] + ', <span style="">' + values[i] + '%</span>'
+      }
+   };
+      
+    var makeBarWithBar = function (gauge, radius, i, width) {
+      var stroke = null;
+        gauge
+          .label(i)
+          .text(dinamicText(i))
+          .useHtml(true);
+        gauge
+          .label(i)
+          .hAlign('center')
+          .vAlign('middle')
+          .anchor('right-center')
+          .padding(0, 10)
+          .height(width / 2 + '%')
+          .offsetY(radius + '%')
+          .offsetX(0);
+        
+        gauge
+          .bar(i)
+          .dataIndex(i)
+          .radius(radius)
+          .width(width)
+          .fill(palette.itemAt(i))
+          .stroke(null)
+          .zIndex(5);
+        gauge
+          .bar(i + 100)
+          .dataIndex(5)
+          .radius(radius)
+          .width(width)
+          .fill('#F5F4F4')
+          .stroke(stroke)
+          .zIndex(4);
+        
+          return gauge.bar(i);
+        };
+        
+        anychart.onDocumentReady(function () {
+          var gauge = anychart.gauges.circular();
+          gauge.data(dataSet);
+          gauge
+            .fill('#fff')
+            .stroke(null)
+            .padding(0)
+            .margin(100)
+            .startAngle(0)
+            .sweepAngle(270);
+        
+          var axis = gauge.axis().radius(100).width(1).fill(null);
+          axis
+            .scale()
+            .minimum(0)
+            .maximum(100)
+            .ticks({ interval: 1 })
+            .minorTicks({ interval: 1 });
+          axis.labels().enabled(false);
+          axis.ticks().enabled(false);
+          axis.minorTicks().enabled(false);
+          makeBarWithBar(gauge, 100, 0, 17);
+          makeBarWithBar(gauge, 80, 1, 17);
+          makeBarWithBar(gauge, 60, 2, 17);
+          makeBarWithBar(gauge, 40, 3, 17);
+          makeBarWithBar(gauge, 20, 4, 17);
+        
+          gauge.margin(50);
+          gauge
+            .title()
+            .text(
+              'Daily % of Micronutrient values on' + ' ' +capitalizeFirstLetter(itemName) +
+              '<br/> <span style="color:#929292; font-size: 5px;"></span>'  + 'from ' +
+              (capitalizeFirstLetter(restaurantName))
+            )
+            .useHtml(true);
+          gauge
+            .title()
+            .enabled(true)
+            .hAlign('center')
+            .padding(0)
+            .margin([0, 0, 20, 0]);
+          
+          var tooltip = gauge.tooltip();
+          gauge.container('radialChartB');
+          gauge.draw();
+        
+        });
+      });
+
+//////////////////// DEFAULT STACKED BAR CHART ////////////////////
+
+d3.json("/api/v1.0/nutrition").then(function(data) {
+  
+  defaultInputA = "egg mcmuffin";
+  defaultInputB = "egg white delight";
+
+  var names = data.names;
+    
+  // Find the index for input for both items
+  itemIndexA = names.indexOf(defaultInputA );
+  itemIndexB = names.indexOf(defaultInputB);
+
+  // Find the items in the Nutrition object
+  itemNutritionInfoA = data.nutrition[itemIndexA];
+  itemNutritionInfoB = data.nutrition[itemIndexB];
+
+  // Select the macronutrient values for both items
+  nutritionalValuesA = itemNutritionInfoA["nutritional_values"];
+  nutritionalValuesB = itemNutritionInfoB["nutritional_values"];
+
+  // Create an array for each item's macronutrient values
+  var carbohydratesA = nutritionalValuesA["carbohydrates_g"];
+  var carbohydratesB = nutritionalValuesB["carbohydrates_g"];
+  var fiberA = nutritionalValuesA["dietary_fiber_g"];
+  var fiberB = nutritionalValuesB["dietary_fiber_g"];
+  var proteinA = nutritionalValuesA["protein_g"];
+  var proteinB = nutritionalValuesB["protein_g"];
+  var saturatedFatA = nutritionalValuesA["saturated_fat_g"];
+  var saturatedFatB = nutritionalValuesB["saturated_fat_g"];
+  var sugarA = nutritionalValuesA["sugars_g"];
+  var sugarB = nutritionalValuesB["sugars_g"];
+  var fatA = nutritionalValuesA["total_fat_g"];
+  var fatB = nutritionalValuesB["total_fat_g"];
+   
+  // Create an array for each macronutrient (containing both item's values)
+  var carbArray = ['Carbohydrates', -carbohydratesA, carbohydratesB];
+  var fiberArray = ['Fiber', -fiberA, fiberB];
+  var proteinArray = ['Protein', -proteinA, proteinB];
+  var satFatArray = ['Saturated Fat', -saturatedFatA, saturatedFatB];
+  var sugarArray = ['Sugar', -sugarA, sugarB];
+  var fatArray = ['Fat', -fatA, fatB];
+
+  // Put each array into one list
+  var dataArray = [carbArray, fiberArray, proteinArray, satFatArray, sugarArray, fatArray];
+
+  // Define data with anychart
+  var dataSet = anychart.data.set(dataArray);
+
+  // Map data for the first series, take x from the zero column and value from the first column of data set
+  var firstSeriesData = dataSet.mapAs({ x: 0, value: 1 });
+
+  // Map data for the second series, take x from the zero column and value from the second column of data set
+  var secondSeriesData = dataSet.mapAs({ x: 0, value: 2 });
+
+  // Create bar chart
+  var chart = anychart.bar();
+
+  // Turn on chart animation
+  chart.animation(true);
+
+  // Set padding
+  chart.padding([10, 20, 5, 20]);
+
+  // Force chart to stack values by Y scale.
+  chart.yScale().stackMode('value');
+  chart.yScale().minimum(50 * -1);
+  chart.yScale().maximum(50);
+
+  // Format y axis labels so they are always positive
+  chart
+    .yAxis()
+    .labels()
+    .format(function () {
+      return Math.abs(this.value).toLocaleString();
+    });
+
+  // Set title for Y-axis
+  chart.yAxis(0).title('Nutritional Value in Grams');
+
+  // Allow labels to overlap
+  chart.xAxis(0).overlapMode('allow-overlap');
+
+  // Turn on extra axis for the symmetry
+  chart
+    .xAxis(1)
+    .enabled(true)
+    .orientation('right')
+    .overlapMode('allow-overlap');
+
+  // Set chart title text
+  chart.title('Macronutrients: Comparison Chart');
+
+  chart.interactivity().hoverMode('by-x');
+
+  chart
+    .tooltip()
+    .title(false)
+    .separator(false)
+    .displayMode('separated')
+    .positionMode('point')
+    .useHtml(true)
+    .fontSize(12)
+    .offsetX(5)
+    .offsetY(0)
+    .format(function () {
+      return (
+        Math.abs(this.value).toLocaleString()
+      );
+   });
+
+  // Temp variable to store series instance
+  var series;
+
+  // Create first series with mapped data
+  series = chart.bar(firstSeriesData);
+  series.name(capitalizeFirstLetter(defaultInputA)).color('HotPink');
+  series.tooltip().position('right').anchor('left-center');
+
+  // Create second series with mapped data
+  series = chart.bar(secondSeriesData);
+  series.name(capitalizeFirstLetter(defaultInputB));
+  series.tooltip().position('left').anchor('right-center');
+
+  // Turn on legend
+  chart
+    .legend()
+    .enabled(true)
+    .inverted(false)
+    .fontSize(13)
+    .padding([0, 0, 20, 0]);
+
+  // Set container id for the chart
+  chart.container('container');
+
+  // Initiate chart drawing
+  chart.draw();
+
+});
